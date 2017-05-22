b64266ef5c75c4c940f8ca993a521ddcc5e9e6f7
ssusoft17kimdonghyun <s6580859@naver.com>
2017-05-22 23:15:36 +0900

    insertion name

--- project.c
+++ project.c
@@ -2,5 +2,23 @@
 #include <stdio.h>
 int main(void)
 {
+	char player_name[10];
+	
+	printf("Start....\ninput name : ");
+	
+	for (int i = 0; (player_name[i-1] != '\n') && (i < 10); i++)
+	{
+		player_name[i] = getchar();
+	}
+
+	printf("\nHello ");
+	
+	for (int i = 0; i < sizeof(player_name); i++)
+	{
+		putchar(player_name[i]);
+	}
+	
+	printf("\n");
+	
 	return 0;
 }
